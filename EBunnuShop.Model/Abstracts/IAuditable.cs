cc0bf3d5c7669f1b63dbc656055b3a0e3851c565
phantanhiep <phantanhiepnt@gmail.com>
2018-08-12 14:57:50 +0700

    Tạo Interface chung cho các model về product

---
+++ EBunnuShop.Model/Abstracts/IAuditable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBunnuShop.Model.Abstracts
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        [MaxLength(256)]
+        string CreatedBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        [MaxLength(256)]
+        string UpdatedBy { set; get; }
+        [MaxLength(256)]
+        string MetaKeyword { set; get; }
+        [MaxLength(256)]
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+}
